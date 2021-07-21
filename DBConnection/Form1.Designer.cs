96b92fea02f0b2de24154166b4c6e2a6deb08240
vonrimS <mr.mytab@gmail.com>
2021-07-22 02:41:15 +0300

    Praxis2 Exercise2

--- DBConnection/Form1.Designer.cs
+++ DBConnection/Form1.Designer.cs
@@ -36,6 +36,9 @@ namespace DBConnection
             this.connectionListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,7 +48,7 @@ namespace DBConnection
             this.файлToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(355, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(217, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -84,7 +87,7 @@ namespace DBConnection
             // 
             this.button1.Location = new System.Drawing.Point(16, 93);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.Size = new System.Drawing.Size(180, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Сколько продуктов";
             this.button1.UseVisualStyleBackColor = true;
@@ -99,11 +102,40 @@ namespace DBConnection
             this.label1.TabIndex = 2;
             this.label1.Text = "Сколько продуктов";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Список продуктов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(16, 183);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(180, 170);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название продукта";
+            this.columnHeader1.Width = 132;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(355, 450);
+            this.ClientSize = new System.Drawing.Size(217, 371);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
@@ -126,6 +158,9 @@ namespace DBConnection
         private System.Windows.Forms.ToolStripMenuItem connectionListToolStripMenuItem;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
     }
 }
 
--- DBConnection/Form1.cs
+++ DBConnection/Form1.cs
@@ -116,5 +116,22 @@ namespace DBConnection
             int number = (int)command.ExecuteScalar();
             label1.Text = number.ToString();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                MessageBox.Show("Сначала подключитесь к базе");
+                return;
+            }
+            OleDbCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT ProductName FROM Products";
+            OleDbDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                listView1.Items.Add(reader["ProductName"].ToString());
+            }
+
+        }
     }
 }

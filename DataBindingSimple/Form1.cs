bbbbb01c8b5ee47b5c5b270c43706db1ae3fbce5
vonrimS <mr.mytab@gmail.com>
2021-07-27 00:14:09 +0300

    Praxis5 Exercise2

---
+++ DataBindingSimple/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBindingSimple
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'northwindDataSet.Products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.northwindDataSet.Products);
+
+            productsBindingSource = new BindingSource(northwindDataSet, "Products");
+
+            ProductIDTextBox.DataBindings.Add("Text", productsBindingSource, "ProductID");
+            ProductNameTextBox.DataBindings.Add("Text", productsBindingSource, "ProductName");
+
+
+        }
+
+        private void PreviousButton_Click(object sender, EventArgs e)
+        {
+            productsBindingSource.MovePrevious();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            productsBindingSource.MoveNext();
+        }
+    }
+}

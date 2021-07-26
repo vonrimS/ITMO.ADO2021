29ba62886bc3805ec6a5e8a3fff3756bbe9cfc98
vonrimS <mr.mytab@gmail.com>
2021-07-26 16:43:04 +0300

    Praxis4 Exercise1

---
+++ DatasetDesigner/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace DatasetDesigner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GetCustomersButton = new System.Windows.Forms.Button();
+            this.CustomersListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // GetCustomersButton
+            // 
+            this.GetCustomersButton.Location = new System.Drawing.Point(33, 37);
+            this.GetCustomersButton.Name = "GetCustomersButton";
+            this.GetCustomersButton.Size = new System.Drawing.Size(125, 23);
+            this.GetCustomersButton.TabIndex = 0;
+            this.GetCustomersButton.Text = "Get Customers";
+            this.GetCustomersButton.UseVisualStyleBackColor = true;
+            this.GetCustomersButton.Click += new System.EventHandler(this.GetCustomersButton_Click);
+            // 
+            // CustomersListBox
+            // 
+            this.CustomersListBox.FormattingEnabled = true;
+            this.CustomersListBox.Location = new System.Drawing.Point(33, 86);
+            this.CustomersListBox.Name = "CustomersListBox";
+            this.CustomersListBox.Size = new System.Drawing.Size(744, 342);
+            this.CustomersListBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CustomersListBox);
+            this.Controls.Add(this.GetCustomersButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GetCustomersButton;
+        private System.Windows.Forms.ListBox CustomersListBox;
+    }
+}
+

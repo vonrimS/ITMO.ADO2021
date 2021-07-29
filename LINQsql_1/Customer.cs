8d5ccc54a21263919c33941c906691e344e97b3b
vonrimS <mr.mytab@gmail.com>
2021-07-29 03:35:58 +0300

    Praxis8 Exercise1

---
+++ LINQsql_1/Customer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQsql_1
+{
+    [Table(Name = "Customers")]
+
+    class Customer
+    {
+        private string _CustomerID;
+        [Column(IsPrimaryKey = true, Storage = "_CustomerID")]
+        public string CustomerID
+        {
+            get
+            {
+                return this._CustomerID;
+            }
+            set
+            {
+                this._CustomerID = value;
+            }
+        }
+
+        private string _City;
+
+        [Column(Storage = "_City")]
+        public string City
+        {
+            get
+            {
+                return this._City;
+            }
+            set
+            {
+                this._City = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return CustomerID + "\t" + City;
+        }
+
+
+    }
+}
---
+++ LINQsql_1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+
+namespace LINQsql_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataContext db = new DataContext (@"Data Source=(local);Initial Catalog=Northwind;Integrated Security=True");
+
+            var results = from c in db.GetTable<Customer>()
+                          where c.City == "London"
+                          select c;
+            foreach (var c in results)
+                listBox1.Items.Add(c.ToString());
+
+        }
+    }
+}
